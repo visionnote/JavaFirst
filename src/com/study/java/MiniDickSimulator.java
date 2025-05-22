facb1aed102ca3ac9b1297910f2ef06a348ded0d
visionnote <visionnote@naver.com>
2025-05-22 17:21:28 +0900

    Merge remote-tracking branch 'origin/master'

--- src/com/study/java/MiniDickSimulator.java
+++ src/com/study/java/MiniDickSimulator.java
@@ -5,5 +5,10 @@ public class MiniDickSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
